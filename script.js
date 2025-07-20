24fc3f7c9de5236415ed073138692370dc045619
MIchowdhury <masum.iqbal.chowdhury@gmail.com>
2025-07-21 00:31:02 +0600

    I have made some changes to some files.

--- script.js
+++ script.js
@@ -716,3 +716,9 @@ if (typeof darkModeBtn !== "undefined") {
     }, 300);
   });
 }
+
+// Set dynamic year in footer
+const footerYear = document.getElementById("footer-year");
+if (footerYear) {
+  footerYear.textContent = new Date().getFullYear();
+}
